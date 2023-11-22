6291d061154d2659cc71084dea6b442cd4694e5a
Kwon Seojin <seojin3154@naver.com>
2023-11-22 22:28:18 +0900

    basic dice app

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:login_example/screens/s_login.dart';
 
 void main() {
-  runApp(
-    MyApp(),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
